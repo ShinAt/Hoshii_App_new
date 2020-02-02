c9ab8181b2bbfdcbeae595b6f089c0b487f8046a
ShinAt <sinntarou836271@gmail.com>
2020-02-03 02:15:21 +0900

    edit home.html

--- spec/controllers/top_page_controller_spec.rb
+++ spec/controllers/top_page_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe TopPageController, type: :controller do
 
-  describe "GET #home" do
+  describe "GET home" do
     it "returns http success" do
       get :home
       expect(response).to have_http_status(:success)
