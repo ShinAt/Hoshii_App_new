146d7662c4795aaf5dfa96f2f8e1a8b5c1452462
ShinAt <sinntarou836271@gmail.com>
2020-02-03 02:35:44 +0900

    add title

--- spec/controllers/top_page_controller_spec.rb
+++ spec/controllers/top_page_controller_spec.rb
@@ -2,10 +2,13 @@ require 'rails_helper'
 
 RSpec.describe TopPageController, type: :controller do
 
+  render_views
+  
   describe "GET home" do
     it "returns http success" do
       get :home
       expect(response).to have_http_status(:success)
+      assert_select "title", "Home | Hoshii!!"
     end
   end
 
